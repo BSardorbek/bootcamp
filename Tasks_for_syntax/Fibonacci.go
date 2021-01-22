8bbaf5466e18e9fcf7fe3d62877bc9723eca5f14
BSardorbek <tuitfb@gmail.com>
2021-01-22 14:41:13 +0500

    fibonacci

--- Tasks_for_syntax/Fibonacci.go
+++ Tasks_for_syntax/Fibonacci.go
@@ -1,5 +1,46 @@
 package main
 
+import "fmt"
+
+func FibonacciClosure() func() int {
+
+	f1, f2 := 0, 1
+	//fn = fn-1 + fn-2
+	return func() int {
+		f1, f2 = f2, f1+f2
+		return f1
+	}
+
+}
+
+func FibonacciRecursion(i int) int {
+
+	if i == 0 {
+		return 0
+	}
+	if i == 1 {
+		return 1
+	}
+
+	return FibonacciRecursion(i-1) + FibonacciRecursion(i-2)
+}
+
 func main() {
+	fmt.Print("Son kiriting ")
+	var n int
+
+	fmt.Scanf("%d", &n)
+
+	fc := FibonacciClosure()
+
+	for i := 0; i < n; i++ {
+		fmt.Println(fc())
+	}
+
+	println("=========")
+
+	for i := 0; i <= n; i++ {
+		fmt.Println(FibonacciRecursion(i))
+	}
 
 }
